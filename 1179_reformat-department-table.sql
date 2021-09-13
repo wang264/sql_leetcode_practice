8f99b22fe5f88a109a4bbffc3df28338cd2709f0
Andy <wang264@hotmail.com>
2021-09-13 13:05:20 -0400

    2021/09/12

--- 1179_reformat-department-table.sql
+++ 1179_reformat-department-table.sql
@@ -49,4 +49,30 @@
 -- insert into Department (id, revenue, month) values ('1', '7000', 'Feb');
 -- insert into Department (id, revenue, month) values ('1', '6000', 'Mar');
 
+SELECT 
+	d.id, 
+	sum(if(d.month = 'Jan', d.revenue, null)) as Jan_Revenue,
+    sum(if(d.month = 'Feb', d.revenue, null)) as Feb_Revenue,
+    sum(if(d.month = 'Mar', d.revenue, null)) as Mar_Revenue,
+    sum(if(d.month = 'Apr', d.revenue, null)) as Apr_Revenue,
+    sum(if(d.month = 'May', d.revenue, null)) as May_Revenue,
+    sum(if(d.month = 'Jun', d.revenue, null)) as Jun_Revenue,
+    sum(if(d.month = 'Jul', d.revenue, null)) as Jul_Revenue,
+    sum(if(d.month = 'Aug', d.revenue, null)) as Aug_Revenue,
+    sum(if(d.month = 'Sep', d.revenue, null)) as Sep_Revenue,
+    sum(if(d.month = 'Oct', d.revenue, null)) as Oct_Revenue,
+    sum(if(d.month = 'Nov', d.revenue, null)) as Nov_Revenue,
+    sum(if(d.month = 'Dec', d.revenue, null)) as Dec_Revenue
+FROM Department d
+GROUP BY d.id;
+
+
+-- select * 
+-- from Department
+-- Where month = 'Jan';
+
+
+
+
+
 
